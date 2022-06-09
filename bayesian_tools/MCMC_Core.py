c8702afc810b0a3e42cb830596a154b6215f94ef
Choi Seokjun <Choi.Seokjun@outlook.com>
2022-06-09 03:54:48 -0700

    bayesian tools: MCMC_diag hist type: density

--- bayesian_tools/MCMC_Core.py
+++ bayesian_tools/MCMC_Core.py
@@ -270,7 +270,7 @@ class MCMC_Diag:
     def show_hist_specific_dim(self, dim_idx, show=False, hist_type="bar"):
         hist_data = self.get_specific_dim_samples(dim_idx)
 
-        plt.hist(hist_data, bins=100, histtype=hist_type)
+        plt.hist(hist_data, bins=100, histtype=hist_type, density=True)
         if self.graphic_use_variable_name:
             plt.ylabel(self.variable_names[dim_idx])
         else:
